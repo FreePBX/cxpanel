de1318670bbbf0e032a17425b82b8b39e30dabde
Andrew Nagy <andrew.nagy@the159.com>
2015-01-06 17:00:54 -0500

    fixes hook for userman

--- functions.inc.php
+++ functions.inc.php
@@ -51,14 +51,16 @@ if(!function_exists('setup_userman')){
 	} else {
 		//dont do anymore work, we need userman and it needs to be enabled
 	}
-	if(function_exists('setup_userman')) {
-		try {
-			$userman = setup_userman();
-			$userman->registerHook('addUser','cxpanel_userman_add');
-        		$userman->registerHook('updateUser','cxpanel_userman_update');
-		} catch(\Exception $e) {
-        		//dont do anymore work, we need userman and it needs to be enabled
-		}
+}
+
+if(function_exists('setup_userman')) {
+	try {
+		$userman = setup_userman();
+		$userman->registerHook('addUser','cxpanel_userman_add');
+		$userman->registerHook('updateUser','cxpanel_userman_update');
+	} catch(\Exception $e) {
+		//dont do anymore work, we need userman and it needs to be enabled
+		return;
 	}
 }
 
