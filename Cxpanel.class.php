e7b2b9b77ca3074e0e43fa18282367bb46dd552f
Andrew Nagy <andrew.nagy@the159.com>
2015-02-02 17:02:08 -0500

    use branding file

---
+++ Cxpanel.class.php
@@ -0,0 +1,85 @@
+<?php
+// vim: set ai ts=4 sw=4 ft=php:
+//
+namespace FreePBX\modules;
+class Cxpanel implements \BMO {
+	public function __construct($freepbx = null) {
+		if ($freepbx == null) {
+			throw new \Exception("Not given a FreePBX Object");
+		}
+		$this->freepbx = $freepbx;
+		$this->userman = $this->freepbx->Userman;
+	}
+
+	public function doConfigPageInit($page) {
+	}
+
+	public function install() {
+
+	}
+	public function uninstall() {
+
+	}
+	public function backup(){
+
+	}
+	public function restore($backup){
+
+	}
+	public function genConfig() {
+	}
+
+	/**
+	 * Hook functionality from userman when a user is updated
+	 * @param {int} $id      The userman user id
+	 * @param {string} $display The display page name where this was executed
+	 * @param {array} $data    Array of data to be able to use
+	 */
+	public function usermanUpdateUser($id, $display, $data) {
+		if(!function_exists('cxpanel_get_config')) {
+			include(__DIR__.'/functions.inc.php');
+		}
+		cxpanel_userman_update($id, $display, $data);
+		$isUser = $this->userman->getModuleSettingByID($id, 'cxpanel', 'add');
+		if($isUser) {
+			exec("php " . $amp_conf['AMPWEBROOT'] . "/admin/modules/cxpanel/modify.php > /dev/null 2>/dev/null &");
+		}
+	}
+
+	/**
+	 * Hook functionality for sending an email from userman
+	 * @param {int} $id      The userman user id
+	 * @param {string} $display The display page name where this was executed
+	 * @param {array} $data    Array of data to be able to use
+	 */
+	public function usermanSendEmail($id, $display, $data) {
+		if(!function_exists('cxpanel_get_config')) {
+			include(__DIR__.'/functions.inc.php');
+		}
+		include(__DIR__."/brand.php");
+		$cxpanelBrandName = !empty($cxpanelBrandName) ? $cxpanelBrandName : "iSymphony";
+
+		$isUser = $this->userman->getModuleSettingByID($id, 'cxpanel', 'add');
+
+		if(!$isUser) {
+			return array();
+		}
+		//Collect email settings and user data
+		$serverInformation = cxpanel_server_get();
+
+		/*
+		* If set utilize the client_host stored in the database else utilize the host
+		* from the current URL.
+		*/
+		$clientHost = $serverInformation['client_host'];
+		if($clientHost == "") {
+			$httpHost = explode(':', $_SERVER['HTTP_HOST']);
+			$clientHost = $httpHost[0];
+		}
+
+
+		$final = array();
+		$final[] = "\t".sprintf(_('%s Login: %s'), $cxpanelBrandName, 'http://' . $clientHost . ':' . $serverInformation['client_port'] . '/client/client');
+		return $final;
+	}
+}
