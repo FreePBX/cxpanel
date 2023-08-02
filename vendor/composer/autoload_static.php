e5047e7d3eb757b489aa8d1f663d77f637e353dc
kapil gupta <kgupta@sangoma.com>
2023-08-02 11:06:38 +0000

    [Module Tag script: cxpanel 16.0.6] FEEPBX-24136

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -24,11 +24,22 @@ class ComposerStaticInite2bea77fe84c2734e077ce15af76f7d3
         ),
     );
 
+    public static $classMap = array (
+        'PHPMailer\\PHPMailer\\DSNConfigurator' => __DIR__ . '/..' . '/phpmailer/phpmailer/src/DSNConfigurator.php',
+        'PHPMailer\\PHPMailer\\Exception' => __DIR__ . '/..' . '/phpmailer/phpmailer/src/Exception.php',
+        'PHPMailer\\PHPMailer\\OAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/src/OAuth.php',
+        'PHPMailer\\PHPMailer\\OAuthTokenProvider' => __DIR__ . '/..' . '/phpmailer/phpmailer/src/OAuthTokenProvider.php',
+        'PHPMailer\\PHPMailer\\PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/src/PHPMailer.php',
+        'PHPMailer\\PHPMailer\\POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/src/POP3.php',
+        'PHPMailer\\PHPMailer\\SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/src/SMTP.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInite2bea77fe84c2734e077ce15af76f7d3::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInite2bea77fe84c2734e077ce15af76f7d3::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInite2bea77fe84c2734e077ce15af76f7d3::$classMap;
 
         }, null, ClassLoader::class);
     }
